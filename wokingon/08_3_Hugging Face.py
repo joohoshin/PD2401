25f5d8a92e07bcc5172078be47025739fa07f6f4
joohoshin <finelloai@gmail.com>
2024-05-13 16:14:39 +0900

    작업 중

---
+++ wokingon/08_3_Hugging Face.py
@@ -0,0 +1,18 @@
+# * 다양한 모델을 공유하는 많은 사이트가 있습니다. 
+#   Pytorch에서 제공되는 pretrained model은 제한적이므로 필요한 내용에 따라 다른 사이트를 활용할 수 있습니다.
+
+#   - Pytorch Hub: https://pytorch.org/hub/
+#   - Hugging Face: https://huggingface.co/
+#   - OpenMMLab: https://github.com/open-mmlab
+
+
+# 시계열 예측 모델을 만들기 위해서 구글의 최신 Foundation 모델인 TimesFM 모델을 허깅페이스에서 사용해봅시다.
+
+# Paper: https://arxiv.org/pdf/2310.10688
+# 모델(checkpoint): https://huggingface.co/google/timesfm-1.0-200m
+
+
+# Install in Terminal
+# git clone https://github.com/google-research/timesfm.git
+#       cpu 버전은 environment_cpu.yml 로 설치
+
